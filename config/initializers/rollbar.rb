0736bfc482b3c4b5f5936ca641f9aedecd94f707
Peter Graham <peterghm@gmail.com>
2017-09-20 21:15:48 -0400

    enable rollbar by default, but only initialize if access token present

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,7 +1,9 @@
 require 'rollbar/rails'
 
-Rollbar.configure do |config|
-  config.access_token = Rails.application.secrets.rollbar_access_token
-  config.enabled = Rails.application.secrets.rollbar_enabled == 'true'
-  config.environment = Rails.application.secrets.app_env
+if Rails.application.secrets.rollbar_access_token.present?
+  Rollbar.configure do |config|
+    config.access_token = Rails.application.secrets.rollbar_access_token
+    config.enabled = Rails.application.secrets.rollbar_enabled == 'true'
+    config.environment = Rails.application.secrets.app_env
+  end
 end
