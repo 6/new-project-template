3c08ac63d00959210d086752adeb5ddd1aa221ae
Peter Graham <peterghm@gmail.com>
2017-12-03 14:43:21 -0500

    fix directory

--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -1,5 +1,5 @@
 describe "template" do
   it "runs" do
-    system("rails new new-project-example -m ~/repo/new-project-template/template/template.rb -d postgresql -T --skip-turbolinks --webpack=react") or raise "Something went wrong"
+    system("rails new new-project-example -m ~/new-project-template/template/template.rb -d postgresql -T --skip-turbolinks --webpack=react") or raise "Something went wrong"
   end
 end
